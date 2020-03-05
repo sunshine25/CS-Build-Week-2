9eb535b54614f1fe4cb4d7811c430a84c69feea1
zoverlvx <zoverhulser@gmail.com>
2020-03-04 19:06:47 -0500

    Examines an item or person.

---
+++ examine.sh
@@ -0,0 +1,4 @@
+curl -X POST -H 'Authorization: Token f976aafe27b40a76a602b84c15df748b3c6d7403' \
+-H "Content-Type: application/json" \
+-d '{"name": "'"$1"'"}' \
+https://lambda-treasure-hunt.herokuapp.com/api/adv/examine/
